1bee11d7c140441b22f413181f8009bdc8f1b7d0
Chris Constatnine <cconstan@gmail.com>
2025-05-11 11:25:15 -0700

    fix

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,12 +1,11 @@
 
-
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(not(feature = "wasm"))]
 pub fn foo(b: bool) -> bool {
     println!("not wasm");
     b
 }
 
-#[cfg(target_arch = "wasm32")]
+#[cfg(feature = "wasm")]
 pub fn foo(i: i32) -> i32 {
     println!("wasm");
     i
